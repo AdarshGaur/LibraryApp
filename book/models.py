436369d4151648f08fbbe678f33ef5013fde3b0d
Adarsh <adarshgaur3@gmail.com>
2020-09-14 01:44:35 +0530

    added templates and model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length = 50)
+    picture = models.ImageField()
+    author = models.CharField(max_length = 30, default='anonymous')
+    email = models.EmailField(blank = True)
+    describe = models.TextField(default = 'Django CRUD tutorial')
+
+    def __str__(self):
+        return self.name
--- libraryapp/settings.py
+++ libraryapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,4 +124,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
